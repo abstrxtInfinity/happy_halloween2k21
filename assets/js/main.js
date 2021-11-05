81c17a07a264ecf4c44f2907cb7df5dbdb54abf1
abstrxtinfinity <anantverma2001@gmail.com>
2021-11-05 19:22:27 +0530

    website done

--- assets/js/main.js
+++ assets/js/main.js
@@ -93,3 +93,19 @@ function scrollUp() {
         scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
+
+
+// ! Scroll Reveal Animation
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    // reset: true
+})
+
+sr.reveal('.home-swiper , .new-swiper , .newsletter__container')
+sr.reveal('.category__data , .trick__content , .footer__content', { interval: 100 })
+sr.reveal('.about__data , .discount__img', { origin: 'left' })
+sr.reveal('.about__img , .discount__data', { origin: 'right' })
